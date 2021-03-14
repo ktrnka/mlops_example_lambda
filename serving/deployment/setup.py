9a7649ff48f757668a4084503f47cad90e1063da
Keith Trnka <keith.trnka@gmail.com>
2021-03-14 13:18:33 -0700

    I don't think these are appropriate since it's not a package to be published on pypi

--- serving/deployment/setup.py
+++ serving/deployment/setup.py
@@ -25,23 +25,4 @@ setuptools.setup(
     ],
 
     python_requires=">=3.6",
-
-    classifiers=[
-        "Development Status :: 4 - Beta",
-
-        "Intended Audience :: Developers",
-
-        "License :: OSI Approved :: Apache Software License",
-
-        "Programming Language :: JavaScript",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-
-        "Topic :: Software Development :: Code Generators",
-        "Topic :: Utilities",
-
-        "Typing :: Typed",
-    ],
 )
