5118ad42d40a184f6d37ad390e68e9f32dcb9315
Keith Trnka <keith.trnka@gmail.com>
2021-03-14 14:49:35 -0700

    Basic tests to make sure the lambda handler works. These tests aren't running in the exact lambda environment so they may not catch all bugs

---
+++ serving/tests/test_lambda_handler.py
@@ -0,0 +1,11 @@
+import json
+import unittest
+from app import lambda_handler
+
+
+class BasicTests(unittest.TestCase):
+    def test_basic_path(self):
+        lambda_handler({"body": json.dumps({"text": "example input"})}, None)
+
+    def test_crash(self):
+        self.assertRaises(BaseException, lambda_handler, {"body": json.dumps({"not_the_right_one": "example input"})}, None)
