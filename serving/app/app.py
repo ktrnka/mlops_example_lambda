f0faa6eddd4d34f28cd889e66be9d245fcab8840
Keith Trnka <keith.trnka@gmail.com>
2021-10-16 13:48:41 -0700

    The order of decorators is significant for staticmethod

--- serving/app/app.py
+++ serving/app/app.py
@@ -16,19 +16,18 @@ class EmfMetrics:
         # This speeds up unit tests; otherwise it auto-detects and tries to connect to HTTP sockets
         metrics_config.environment = os.environ.get("AWS_EXECUTION_ENV", "local")
 
-    @metric_scope
     @staticmethod
+    @metric_scope
     def put_duration(name: str, duration_seconds: float, metrics: MetricsLogger):
         metrics.put_metric(name, duration_seconds, "Seconds")
 
-    @metric_scope
     @staticmethod
+    @metric_scope
     def put_count(name: str, count: int, metrics: MetricsLogger):
         metrics.put_metric(name, count, "Count")
 
 
-@metric_scope
-def load_model(metrics: MetricsLogger):
+def load_model():
     start_time = time.time()
     model_path = os.path.join(os.path.dirname(__file__), "data/model.joblib.gz", )
     model = joblib.load(model_path)
@@ -39,11 +38,11 @@ def load_model(metrics: MetricsLogger):
     return model
 
 
+EmfMetrics.setup()
 model = load_model()
 
 
-@metric_scope
-def lambda_handler(event, context, metrics: MetricsLogger):
+def lambda_handler(event, context):
     request_body = json.loads(event["body"])
     prediction = str(model.predict([request_body["text"]])[0])
 
