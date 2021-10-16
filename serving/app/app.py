38126e198541a9fe4886a3e4bff23279f2168e5d
Keith Trnka <keith.trnka@gmail.com>
2021-10-16 13:38:55 -0700

    Tweaked version so I can swap out metrics implementations

--- serving/app/app.py
+++ serving/app/app.py
@@ -6,12 +6,25 @@ import joblib
 from aws_embedded_metrics import metric_scope, MetricsLogger
 from aws_embedded_metrics.config import get_config
 
-metrics_config = get_config()
-metrics_config.namespace = os.environ.get("AWS_LAMBDA_FUNCTION_NAME", metrics_config.namespace)
 
-# The specific env var isn't too important. What's important is that it'll be set on lambda, and local otherwise
-# This speeds up unit tests; otherwise it auto-detects and tries to connect to HTTP sockets
-metrics_config.environment = os.environ.get("AWS_EXECUTION_ENV", "local")
+class EmfMetrics:
+    @staticmethod
+    def setup():
+        metrics_config = get_config()
+        metrics_config.namespace = os.environ.get("AWS_LAMBDA_FUNCTION_NAME", metrics_config.namespace)
+
+        # This speeds up unit tests; otherwise it auto-detects and tries to connect to HTTP sockets
+        metrics_config.environment = os.environ.get("AWS_EXECUTION_ENV", "local")
+
+    @metric_scope
+    @staticmethod
+    def put_duration(name: str, duration_seconds: float, metrics: MetricsLogger):
+        metrics.put_metric(name, duration_seconds, "Seconds")
+
+    @metric_scope
+    @staticmethod
+    def put_count(name: str, count: int, metrics: MetricsLogger):
+        metrics.put_metric(name, count, "Count")
 
 
 @metric_scope
@@ -21,7 +34,7 @@ def load_model(metrics: MetricsLogger):
     model = joblib.load(model_path)
 
     duration_seconds = time.time() - start_time
-    metrics.put_metric("load_model.duration", duration_seconds, "Seconds")
+    EmfMetrics.put_duration("load_model.duration", duration_seconds)
 
     return model
 
@@ -34,7 +47,7 @@ def lambda_handler(event, context, metrics: MetricsLogger):
     request_body = json.loads(event["body"])
     prediction = str(model.predict([request_body["text"]])[0])
 
-    metrics.put_metric("input.num_chars", len(request_body["text"]), "Count")
+    EmfMetrics.put_count("input.num_chars", len(request_body["text"]))
 
     return {
         "statusCode": 200,
