1f680af4464a3bc3e571329277581f6e295ce765
Keith Trnka <keith.trnka@gmail.com>
2021-10-16 09:48:02 -0700

    Setting the namespace from the env vars and tracking model loading time

--- serving/app/app.py
+++ serving/app/app.py
@@ -1,10 +1,28 @@
 import json
 import os.path
+import time
+
 import joblib
 from aws_embedded_metrics import metric_scope, MetricsLogger
+from aws_embedded_metrics.config import get_config
 
-model_path = os.path.join(os.path.dirname(__file__), "data/model.joblib.gz", )
-model = joblib.load(model_path)
+metrics_config = get_config()
+metrics_config.namespace = os.environ.get("AWS_LAMBDA_FUNCTION_NAME", metrics_config.namespace)
+
+
+@metric_scope
+def load_model(metrics: MetricsLogger):
+    start_time = time.time()
+    model_path = os.path.join(os.path.dirname(__file__), "data/model.joblib.gz", )
+    model = joblib.load(model_path)
+
+    duration_seconds = time.time() - start_time
+    metrics.put_metric("load_model.duration", duration_seconds, "Seconds")
+
+    return model
+
+
+model = load_model()
 
 
 @metric_scope
