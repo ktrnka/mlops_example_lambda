7c40cc50a4eaf4e535a262d4924ce8691dcce546
Keith Trnka <keith.trnka@gmail.com>
2021-10-16 09:05:08 -0700

    Testing out an uninteresting, basic metric

--- serving/app/app.py
+++ serving/app/app.py
@@ -1,13 +1,18 @@
 import json
 import os.path
 import joblib
+from aws_embedded_metrics import metric_scope, MetricsLogger
 
 model_path = os.path.join(os.path.dirname(__file__), "data/model.joblib.gz", )
 model = joblib.load(model_path)
 
-def lambda_handler(event, context):
+
+@metric_scope
+def lambda_handler(event, context, metrics: MetricsLogger):
     request_body = json.loads(event["body"])
     prediction = str(model.predict([request_body["text"]])[0])
+
+    metrics.put_metric("input.num_chars", len(request_body["text"]), "Count")
 
     return {
         "statusCode": 200,
